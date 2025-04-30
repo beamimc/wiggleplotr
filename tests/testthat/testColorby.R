36dd10841758f74b0bda630eb747bc0c92346951
Beatriz Campillo Minano (beacm) <beacm@c0300ood18.ll.unc.edu>
2025-04-30 15:56:50 -0400

    include plotTranscripts color_by test

--- tests/testthat/testColorby.R
+++ tests/testthat/testColorby.R
@@ -1,41 +1,40 @@
-library(GenomicRanges)
-devtools::load_all()
-# Create two simple GRanges objects
-gr1 <- GRanges(seqnames = "chr1",
-               ranges = IRanges(start = c(1, 10, 20),
-                                end = c(5, 15, 25)),
-               strand = c("+", "+", "+"))
+context("Color transcripts")
 
-gr2 <- GRanges(seqnames = "chr1",
-               ranges = IRanges(start = c(100, 200),
-                                end = c(150, 250)),
-               strand = c("-", "-"))
-gr3 <- GRanges(seqnames = "chr1",
-               ranges = IRanges(start = c(100, 200),
-                                end = c(150, 250)),
-               strand = c("-", "-"))
-# Now combine them into a GRangesList
-grl <- GRangesList(gr1 = gr1, gr2 = gr2, gr3 = gr3)
 
-# View the GRangesList
-grl
-
-#' transcript_annotations Data frame with at least three columns: transcript_id, gene_name, strand.
-#' Used to construct transcript labels. (default: NULL)
-
-transcript_annotations <- dplyr::tibble(
+test_that("plotTranscripts colors transcripts using color_by column in transcript_annotations",{
+  
+  # Create two simple GRanges objects
+  gr1 <- GenomicRanges::GRanges(seqnames = "chr1",
+                 ranges = IRanges(start = c(1, 10, 20),
+                                  end = c(5, 15, 25)),
+                 strand = c("+", "+", "+"))
+  
+  gr2 <- GenomicRanges::GRanges(seqnames = "chr1",
+                 ranges = IRanges(start = c(100, 200),
+                                  end = c(150, 250)),
+                 strand = c("-", "-"))
+  
+  # Now combine them into a GRangesList
+  grl <- GRangesList(gr1 = gr1, gr2 = gr2)
+  
+  #' transcript_annotations Data frame with at least  columns: transcript_id, strand.
+  #' Used to construct transcript labels. (default: NULL) -- add color_by function
+  
+  transcript_annotations <- dplyr::tibble(
     transcript_id = names(grl),
     strand = sapply(grl, function(gr) {
       strands <- as.character(strand(gr))
       unique_strands <- unique(strands)
-        
-        if (length(unique_strands) == 1) {
-          unique_strands
-        } else {
-          "*"  # if multiple strands found (inconsistent), mark "*"
-        }
-      }),
-    color_by = c('lightgreen','#fdae61','red')
-)
-
-wiggleplotr::plotTranscripts(grl, transcript_annotations = transcript_annotations)
+      
+      if (length(unique_strands) == 1) {
+        unique_strands
+      } else {
+        "*"  # if multiple strands found (inconsistent), mark "*"
+      }
+    }),
+    color_by = c('lightgreen','#fdae61')
+  )
+  
+  wiggleplotr::plotTranscripts(grl, transcript_annotations = transcript_annotations)
+  
+})
